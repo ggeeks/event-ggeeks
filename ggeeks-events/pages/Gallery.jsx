e22caa7699d7a2e75467284460d652fe2dbac931
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-05-20 22:51:51 +0530

    gallery init

--- ggeeks-events/pages/Gallery.jsx
+++ ggeeks-events/pages/Gallery.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Gallery() {
   return (
-    <div>Gallery</div>
+    <div>
+      <h1 className='text-4xl'>Gallery</h1>
+      <div className='px-2 py-7 '>
+        <h1 className='text-2xl'>Our Events</h1>
+        <div>
+          <div className='flex flex-row'>
+            {/* 3x1 */}
+          </div>
+          <div className='flex flex-row'>
+            {/* 3x1 */}
+          </div>
+        </div>
+      </div>
+      <div className='px-2 py-7 '>
+        <h1 className='text-2xl'>Glimpes of Events</h1>
+        <div>
+          <div className='flex flex-row'>
+            {/* 3x1 */}
+          </div>
+          <div className='flex flex-row'>
+            {/* 3x1 */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
