9ea308bb1ab602748bae944c7bafb20283db8843
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-05-20 23:09:09 +0530

    games init

--- ggeeks-events/pages/Games.jsx
+++ ggeeks-events/pages/Games.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Games() {
   return (
-    <div>Games</div>
+    <div className='h-[67vh]'>
+      <div>
+        <h1 className='text-6xl'>Games</h1>
+      </div>
+      <div className='py-24'>
+      <h1 className='text-9xl mx-10'>Coming Soon</h1>
+
+      </div>
+
+    </div>
   )
 }
 
