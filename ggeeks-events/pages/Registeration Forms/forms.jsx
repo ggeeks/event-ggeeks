edac67db2ea094e957a68518aa59c76d207c6aef
yuvraj3905 <yuvraj202001@gmail.com>
2022-05-21 23:44:58 +0530

    Added Forms

---
+++ ggeeks-events/pages/Registeration Forms/forms.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+function Form (){
+    return (
+        // Chess Form
+      <div> 
+        <div><h1 className='text-4xl'>CHESS</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+      //bgmi Form 
+    return(  <div> 
+        <div><h1 className='text-4xl'> BGMI</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <input type="text" placeholder='Team Leader(In Game Name)' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+    return(
+        <div> 
+        <div><h1 className='text-4xl'> Scribble</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+    return (
+        // Valorant
+      <div> 
+        <div><h1 className='text-4xl'>Valorant</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+    return (
+        // Among Us
+      <div> 
+        <div><h1 className='text-4xl'> Among Us</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+    return (
+        // CODM
+      <div> 
+        <div><h1 className='text-4xl'> CODM</h1></div>
+        <div className='px-40 py-10'>
+          <form action="" method="get" className='pt-10 flex flex-col px-10 justify-center '>
+            <input type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
+            <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
+            <input type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' />
+            <button type="submit" className='text-2xl border items-center align-middle rounded-xl justify-center my-3 px-3 py-4 bg-animate w-40 text-center '>Register</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+  export default Form
