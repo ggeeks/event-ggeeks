f715196b5514f554d863e6e2619a191d4f3f87be
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-06-03 21:47:42 +0530

    Added Team Form Backend

--- ggeeks-events/pages/forms/team-form/BgmiTeam.jsx
+++ ggeeks-events/pages/forms/team-form/BgmiTeam.jsx
@@ -4,6 +4,17 @@ import { IoIosArrowBack } from 'react-icons/io'
 import Teams from '../../event/Teams'
 
 const BgmiTeam = () => {
+  // const scriptURL = 'https://script.google.com/macros/s/AKfycbzYJL2GMNCFXPmAVhwZ9Vh_rperXsqsmLrF3ZV_33LwbV4oHI8njGGSyBVXFTDJOi7FIA/exec'
+  // const form = document.forms['BGMI-Form']
+  // form.addEventListener('submit', e => {
+  //     e.preventDefault()
+  //     fetch(scriptURL, {
+  //             method: 'POST',
+  //             body: new FormData(form)
+  //         })
+  //         .then(response => alert("Registration Successfull"))
+  //         .catch(error => console.error('Error!', error.message)) 
+  // })
   return (
     <div className='justify-center'> 
       <Link href='../../event/Teams' to={<Teams/>} alt='teams'>
@@ -14,7 +25,7 @@ const BgmiTeam = () => {
     </Link>
     <div><h1 className='md:text-5xl text-2xl px-5 py-3'> BGMI</h1></div>
     <div className=' md:px-40 md:py-10 px-10 py-3'>
-      <form action="" method="get" className='grid grid-cols-1  md:px-10 px-2 justify-center '>
+      <form name="BGMI-Form"action="" method="POST" className='grid grid-cols-1  md:px-10 px-2 justify-center '>
         <input name="name" type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
         <input name="phone-number" type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
         <input name="email" type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
--- ggeeks-events/pages/forms/team-form/ValorantTeam.jsx
+++ ggeeks-events/pages/forms/team-form/ValorantTeam.jsx
@@ -4,6 +4,17 @@ import { IoIosArrowBack } from 'react-icons/io'
 import Teams from '../../event/Teams'
 
 const ValorantTeam = () => {
+  // const scriptURL = 'https://script.google.com/macros/s/AKfycbwwKlaS7okN4gugnht0UNTDEGrQyDp5KcFzqAL_Y_f8GxATXgCiM6GD8HeE8GaQYWLt/exec'
+  // const form = document.forms['Valorant-Form']
+  // form.addEventListener('submit', e => {
+  //     e.preventDefault()
+  //     fetch(scriptURL, {
+  //             method: 'POST',
+  //             body: new FormData(form)
+  //         })
+  //         .then(response => alert("Registration Successfull"))
+  //         .catch(error => console.error('Error!', error.message)) 
+  // })
   return (
     <div className='justify-center'> 
       <Link href='../../event/Teams' to={<Teams/>} alt='teams'>
@@ -14,7 +25,7 @@ const ValorantTeam = () => {
     </Link>
     <div><h1 className=' md:text-5xl text-2xl px-5 py-3'>Valorant</h1></div>
     <div className='md:px-40 md:py-10 px-10 py-3'>
-    <form action="" method="get" className='grid grid-cols-1  md:px-10 px-2 justify-center'>
+    <form name="Valorant-Form"action="" method="POST" className='grid grid-cols-1  md:px-10 px-2 justify-center'>
         <input name="name" type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
         <input name="phone-number" type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' />
         <input name="email" type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5' />
