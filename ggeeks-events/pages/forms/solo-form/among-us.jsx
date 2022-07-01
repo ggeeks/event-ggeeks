97e30c77a1eb23355d8b71aaf09a58c3b76fae55
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-07-01 14:50:18 +0530

    Updated Form

--- ggeeks-events/pages/forms/solo-form/among-us.jsx
+++ ggeeks-events/pages/forms/solo-form/among-us.jsx
@@ -3,70 +3,118 @@ import React, { useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 import Solo from '../../event/solo'
 import axios from 'axios'
-function AmongUs (){
-  // const scriptURL = 'https://script.google.com/macros/s/AKfycbxdM4v_gkgI_O0vt-qVEO4jfDnCOd8PH5sKfTvMIMb3cm1Oe_II2Rj_WTBxu7dzO4r9/exec'
-  // const form = document.forms['Amongus-Form']
-  // form.addEventListener('submit', e => {
-  //     e.preventDefault()
-  //     fetch(scriptURL, {
-  //             method: 'POST',
-  //             body: new FormData(form)
-  //         })
-  //         .then(response => alert("Registration Successfull"))
-  //         .catch(error => console.error('Error!', error.message)) 
-  // })
+import { Form } from 'react-bootstrap'
+function AmongUs() {
+  const [name, setName] = useState('')
+  const [phoneNumber, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [institute, setInstitute] = useState('')
+  const [discord, setDiscord] = useState('')
 
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [institute, setInstitute] = useState('');
-  const [discord, setDiscord] = useState('');
-
-  const handleSubmit =(e)=>{
-    e.preventDefault();
-    // console.log(name,email,phone,institute,discord);
-    const data ={
-      Name:name,
-      Email:email,
-      Phone:phoneNumber,
-      Institute:institute,
-      Discord:discord
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(name,email,phoneNumber,institute,discord);
+    const data = {
+      name  : name,
+      email: email,
+      phoneNumber: phoneNumber,
+      institute: institute,
+      discord: discord,
     }
-
-    axios.post("https://sheet.best/api/sheets/fb4b2b5c-2de7-40b6-9b23-4bf6c7eec0d8", data).then(response=>{
-      // console.log(response);
-      setName('');
-      setEmail('');
-      setInstitute('');
-      setDiscord('');
-      setPhone('');
-      alert("YOU HAVE BEEN REGISTERED SUCCESSFULLY!");
-      
-    });
-
+    axios
+      .post(
+        'https://sheet.best/api/sheets/fb4b2b5c-2de7-40b6-9b23-4bf6c7eec0d8',
+        // 'https://script.google.com/macros/s/AKfycbxdM4v_gkgI_O0vt-qVEO4jfDnCOd8PH5sKfTvMIMb3cm1Oe_II2Rj_WTBxu7dzO4r9/exec',
+        // 'http://localhost:3300/api/skribbl',
+        data
+      )
+      .then((response) => {
+        // console.log(response);
+        setName('')
+        setEmail('')
+        setInstitute('')
+        setDiscord('')
+        setPhone('')
+        alert('YOU HAVE BEEN REGISTERED SUCCESSFULLY!')
+      })
   }
 
-return (
+  return (
     // Among Us
-  <div className='justify-center'> 
-    <Link href='../../event/solo' to={<Solo/>} alt='solo'>
-    <div className='text-left flex flex-row'>
-          <IoIosArrowBack className='text-xl items-center m-1' />
-          <button className=' text-xl' >Back</button>
+    <div className="justify-center">
+      <Link href="../../event/solo" to={<Solo />} alt="solo">
+        <div className="flex flex-row text-left">
+          <IoIosArrowBack className="m-1 items-center text-xl" />
+          <button className=" text-xl">Back</button>
         </div>
-    </Link>
-    <div ><h1 className='md:text-5xl text-2xl px-5 py-3'> Among Us</h1></div>
-    <div className='md:px-40 md:py-10 px-10 py-3 form-font'>
-      <form name="Amongus-Form" action="" method="POST" className='grid grid-cols-1  md:px-10 px-2 justify-center form-font' onSubmit={handleSubmit}>
-        <input name="name" type="text" placeholder='Name' className='white-glassmorphism my-3 bg-transparent px-3 py-5' required onChange={(e)=>setName(e.target.value)} value={name}/>
-        <input name="phone-number" type="number" placeholder='Phone Number' className='white-glassmorphism my-3 bg-transparent px-3 py-5' required onChange={(e)=>setPhone(e.target.value)} value={phoneNumber}/>
-        <input name="email" type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-5 font-nuito' required onChange={(e)=>setEmail(e.target.value)} value={email}/>
-        <input name="institute" type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-5' required onChange={(e)=>setInstitute(e.target.value)} value={institute}/>
-        <input name="discord" type="text" placeholder='Discord Username' className='white-glassmorphism bg-transparent px-3 my-3 py-5' required onChange={(e)=>setDiscord(e.target.value)} value={discord}/>
-        <div><button type="submit" className='md:text-2xl text-lg blue-glassmorphism hover:text-orange-500 md:w-2/5 w-3/5 justify-center rounded-xl my-3 px-3 py-4'>Register</button></div>
-      </form>
+      </Link>
+      <div>
+        <h1 className="px-5 py-3 text-2xl md:text-5xl"> Among Us</h1>
+      </div>
+      <div className="form-font px-10 py-3 md:px-40 md:py-10">
+        <form
+          name="Amongus-Form"
+          action=""
+          method="POST"
+          className="form-font grid  grid-cols-1 justify-center px-2 md:px-10"
+          onSubmit={handleSubmit}
+        >
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            className="white-glassmorphism my-3 bg-transparent px-3 py-5"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            name="phone-number"
+            type="number"
+            placeholder="Phone Number"
+            className="white-glassmorphism my-3 bg-transparent px-3 py-5"
+            required
+            onChange={(e) => setPhone(e.target.value)}
+            value={phoneNumber}
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            className="white-glassmorphism my-3 bg-transparent px-3 py-5 font-nuito"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            name="institute"
+            type="text"
+            placeholder="Institute"
+            className="white-glassmorphism my-3 bg-transparent px-3 py-5"
+            required
+            onChange={(e) => setInstitute(e.target.value)}
+            value={institute}
+          />
+          <input
+            name="discord"
+            type="text"
+            placeholder="Discord Username"
+            className="white-glassmorphism my-3 bg-transparent px-3 py-5"
+            required
+            onChange={(e) => setDiscord(e.target.value)}
+            value={discord}
+          />
+          <div>
+            <button
+              type="submit"
+              className="blue-glassmorphism my-3 w-3/5 justify-center rounded-xl px-3 py-4 text-lg hover:text-orange-500 md:w-2/5 md:text-2xl"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-)
+  )
 }
 export default AmongUs
