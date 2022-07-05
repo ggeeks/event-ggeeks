46a73f1a3bfc5a0e7e75f827271c9bc33eb07728
vanshika-a <75539206+vanshika-a@users.noreply.github.com>
2022-07-05 14:36:03 +0530

    team deleted

--- ggeeks-events/pages/contact.jsx
+++ ggeeks-events/pages/contact.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 function contact() {
+
+  const [name, setName] = useState('')
+  const [phoneNumber, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [institute, setInstitute] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      Name: name,
+      Email: email,
+      Phone: phoneNumber,
+      Institute: institute,
+      Message:message,
+    }
+
+    axios.post(
+      // 'https://sheet.best/api/sheets/fb4b2b5c-2de7-40b6-9b23-4bf6c7eec0d8',
+      'https://event-website-backend.herokuapp.com/api/bgmiSolo', data).then((response) => {
+        setName('')
+        setEmail('')
+        setInstitute('')
+        setMessage('')
+        setPhone('')
+        alert('THANK YOU FOR YOUR FEEDBACK!')
+      })
+  }
+
+
   return (
     <div className='justify-center'>
       <div><h1 className='md:text-5xl text-2xl px-5 py-3'> Contact Us</h1></div>
       <div className='py-2'>
         <div>
-        <form action="" method="POST" className=' grid grid-cols-1  md:px-10 px-2 justify-center '>
-          <input type="text" placeholder='Name' className='white-glassmorphism bg-transparent my-3 px-3 py-3' />
-          <input type="number" placeholder='Phone Number' className='white-glassmorphism bg-transparent my-3 px-3 py-3 ' />
-          <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-3' />
-          <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-3' />
-          <textarea type="text" placeholder='Message' className='white-glassmorphism bg-transparent px-3 my-3 py-3' />
+        <form action="" method="POST" className=' grid grid-cols-1  md:px-10 px-2 justify-center ' onSubmit={handleSubmit}>
+          <input type="text" placeholder='Name' className='white-glassmorphism bg-transparent my-3 px-3 py-3' required onChange={(e) => setName(e.target.value)} value={name} />
+          <input type="number" placeholder='Phone Number' className='white-glassmorphism bg-transparent my-3 px-3 py-3 ' required onChange={(e) => setPhone(e.target.value)} value={phoneNumber}/>
+          <input type="email" placeholder='E-mail' className='white-glassmorphism bg-transparent my-3 px-3 py-3' required onChange={(e) => setEmail(e.target.value)} value={email} />
+          <input type="text" placeholder='Institute' className='white-glassmorphism bg-transparent my-3 px-3 py-3' required onChange={(e) => setInstitute(e.target.value)} value={institute}/>
+          <textarea type="text" placeholder='Message' className='white-glassmorphism bg-transparent px-3 my-3 py-3' required onChange={(e) => setMessage(e.target.value)} value={message}/>
           <div><button type="submit" className='md:text-2xl text-lg border items-center rounded-xl my-3 px-3 py-4 blue-glassmorphism hover:text-orange-500 md:w-40 w-20 text-center '>Send</button></div>
         </form>
         </div>
--- ggeeks-events/pages/register.jsx
+++ ggeeks-events/pages/register.jsx
@@ -1,18 +1,36 @@
-import Image from 'next/image'
-import Link from 'next/link'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import React from 'react'
+// import individual from '../assets/reg/individual.png'
+// import team from "../assets/reg/team.png"
+// // import Router from 'next/router'
+// import Solo from './event/solo.jsx'
+// import Teams from "./event/team.jsx"
+
 import React from 'react'
-import individual from '../assets/reg/individual.png'
-import team from "../assets/reg/team.png"
-// import Router from 'next/router'
-import Solo from './event/solo.jsx'
-import Teams from "./event/team.jsx"
+// import Register from '../register'
+import Link from 'next/link'
+import { IoIosArrowBack } from 'react-icons/io'
+import stumbleGuys from '../assets/games/stumble-guys.png'
+import bgmi from '../assets/games/bgmi.png'
+import chess from '../assets/games/chess.png'
+import cod from '../assets/games/cod.png'
+import skribbleImage from '../assets/games/skribble.png'
+import valorant from '../assets/games/valorant.png'
+import Image from 'next/image'
+import StumbleGuys from './forms/solo-form/stumble-guys'
+import BgmiSolo from './forms/solo-form/bgmi'
+import ChessSolo from './forms/solo-form/chess'
+import CODMSolo from './forms/solo-form/codm'
+import SkribbleSolo from './forms/solo-form/skribbl'
+import ValorantSolo from './forms/solo-form/valorant'
 
 function Register() {
 
   return (
     <div className='revamp' >
       <div><h1 className='md:text-5xl text-2xl'>Register</h1></div>
-      <div className='flex md:flex-row flex-col justify-evenly'>
+      {/* <div className='flex md:flex-row flex-col justify-evenly'>
         <div className='justify-center'>
           <h1 className='md:text-3xl text-lg px-5 py-3'>
             SOLO
@@ -38,7 +56,96 @@ function Register() {
           </Link>
         </div>
 
+      </div> */}
+
+      <div className="py-5">
+        <div className="grid grid-cols-1 md:grid-cols-3  ">
+          <Link href="../forms/solo-form/chess" to={<ChessSolo />} alt="chess">
+            <div className='m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12'>
+              <div className="blue-glassmorphism m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12">
+              <Image src={chess} height={970} alt="chess-image" />
+              <h2 className="text-xl">Chess</h2>
+              
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500' onClick={()=>(<ChessSolo/>)}>Register</button>
+            </div>
+            
+          </Link>
+          
+          <Link
+            href="../forms/solo-form/stumble-guys"
+            to={<StumbleGuys />}
+            alt="stumble-guys"
+          >
+            <div className='m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12'>
+              <div className="blue-glassmorphism m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12">
+              <Image src={stumbleGuys} height={1020} alt="stumble-guys" />
+              <h2 className="text-xl">Stumble Guys</h2>
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500' onClick={()=>(<ChessSolo/>)}>Register</button>
+            </div>
+            
+          </Link>
+          <Link
+            href="../forms/solo-form/skribbl"
+            alt="skribble"
+            to={<SkribbleSolo />}
+          >
+            <div className='m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12'>
+              <div className="blue-glassmorphism m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12">
+              <Image src={skribbleImage} height={850} alt="skribble-image" />
+              <h2 className="text-xl">Skribbl</h2>
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500'>Register</button>
+            </div>
+            
+          </Link>
+          <Link
+            href="../forms/solo-form/codm"
+            alt="codm-image"
+            to={<CODMSolo />}
+          >
+            <div className='m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12'>
+            <div className="blue-glassmorphism m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12">
+              <Image src={cod} height={950} alt="cod" />
+              <h2 className="text-xl">Call of Duty</h2>
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500'>Register</button>
+            </div>
+          </Link>
+          
+          <Link
+            href="../forms/solo-form/valorant"
+            alt="valorant"
+            to={<ValorantSolo />}
+          >
+            <div className='m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12'>
+            <div className=" blue-glassmorphism m-5 w-5/6 cursor-pointer rounded-2xl px-3 py-2 md:w-11/12">
+              <Image
+                src={valorant}
+                height={680}
+                classname="rounded-2xl"
+                alt="valorant"
+              />
+              <h2 className="text-xl">Valorant</h2>
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500'>Register</button>
+            </div>
+          </Link>
+
+          <Link href="../forms/solo-form/bgmi" to={<BgmiSolo />} alt="BGMI">
+            <div className='m-5 w-5/6  px-3 py-2 md:w-11/12'>
+            <div className="blue-glassmorphism m-5 w-5/6 cursor-pointer px-3 py-2 md:w-11/12">
+              <Image src={bgmi} alt="bgmi" />
+              <h2 className="text-xl">BGMI</h2>
+            </div>
+            <button className=' px-2 py-1  rounded-lg text-xl hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500 bg-gradient-to-r from-purple-500 to-pink-500'>Register</button>
+            </div>
+          </Link>
+
+        </div>
       </div>
+
       <div className=''>
         <h1 className='text-xl md:text-3xl py-10 text-orange-400'>Terms & condition </h1>
         <div className='justify-start text-left para-1 px-20 white-glassmorphism'>
