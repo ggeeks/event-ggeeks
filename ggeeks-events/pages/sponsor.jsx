fe8912541fef3de1feef1491ef31c13872056eed
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-07-11 20:25:01 +0530

    space added

--- ggeeks-events/pages/sponsor.jsx
+++ ggeeks-events/pages/sponsor.jsx
@@ -35,6 +35,17 @@ function Sponser() {
           <div className='p-3'>
             <Image src={revUP} width={250} height={150} alt="RevUp"/>
           </div>
+          {/* <div className='p-3'>
+            <Image src={gfg} width={250} height={150} alt="Geeks for Geeks"/>
+          </div>
+          <div className='px-3'>
+            <Image src={give} width={250} height={150} alt='Give My Certificate'/>
+          </div> */}
+        </div>
+      </div>
+      {/* <div className='py-5 text-3xl'>Title Sponsor</div> */}
+      <div className='p-3 flex '>
+        <div className="grid md:grid-cols-2 grid-cols-1 w-5/6 md:mx-24 mx-12">
           <div className='p-3'>
             <Image src={gfg} width={250} height={150} alt="Geeks for Geeks"/>
           </div>
@@ -43,15 +54,6 @@ function Sponser() {
           </div>
         </div>
       </div>
-      {/* <div className='py-5 text-3xl'>Title Sponsor</div> */}
-      {/* <div className='p-3 '>
-        <div className="grid md:grid-cols-1 grid-cols-1 w-5/6 md:mx-24 mx-12">
-          
-          <div className='px-3'>
-            <Image src={give} width={250} height={150} alt='Give My Certificate'/>
-          </div>
-        </div>
-      </div> */}
       <div className=' text-center pt-5 justify-center'>
         <h1 className='md:text-2xl text-lg items-center my-2 '>Want to sponsor us?</h1>
         <a href="mailto:ggeeks.platform@gmail.com"><button className='rounded-2xl border md:text-2xl transition-300 text-lg white-glassmorphism mx-3 px-2 py-1 hover:text-orange-500 hover:bg-white shadow-md  shadow-orange-500'>Contact us</button></a>
