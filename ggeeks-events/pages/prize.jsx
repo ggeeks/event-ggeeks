bf64ee310712f23553cfc3b5c6fd6fb71ef0b77b
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-07-14 18:31:34 +0530

    prize

--- ggeeks-events/pages/prize.jsx
+++ ggeeks-events/pages/prize.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 
 const prize = () => {
-  return (
-    <div>
-         <h1>Prize</h1>
-        <div className=' flex '>
-           <div>
-            <p>A intriguing prize pool of 15k, with amazing swags such as t-shirts,stickers and coupons from our sponsors..</p>
-           </div>
-           <div>
-            <p>The winner would be awarded with tshirt and coupons and the runners would be gifted with coupon and also the winner would be provided with cool stickers</p>
+    return (
+        <div>
+            <h1 className='text-[36px] md:my-20 my-10 md:text-[46px]'>Prize</h1>
+            <div className=' flex para-1 '>
 
-           </div>
-           <div>
-            <p>All the above facts are sentenced to some conditions which includes that the player should have registered for the event and was present throughout the compitition</p>
-           </div>
+                <ol className='text-left md:text-xl text-[14px]'>
+                    <li>
+                        &gt; A intriguing prize pool of<span className='text-orange-500'> <strong>15K</strong></span>, with amazing swags such as t-shirts, stickers and coupons from our sponsors..
+
+                    </li>
+                    
+
+                    <br />
+                    <li>
+                    &gt; The winner would be awarded with tshirt and coupons and the runner-ups would be gifted with coupon and also the winner would be provided with cool stickers
+
+                    </li>
+                    
+
+                    <br />
+                    <li>
+                    &gt; All the above facts are sentenced to some terms & conditions which includes that the player should have registered for the event and was present throughout the compitition
+
+                    </li>
+                    
+                    <br></br>
+                </ol>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default prize
